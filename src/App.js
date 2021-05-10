704eaec45018cbbbafd249006c3c4b9ee54d70de
AWend01Dev <gohikeco1@gmail.com>
2021-05-10 09:20:01 -0500

    this pr adds fetch and displays api contents on page.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';  
+
+
+const URL = 'https://api.openweathermap.org/data/2.5/onecall?lat=41.878113&lon=-87.629799&appid=a889ab39f32412f8e65caec1d77a1baa'
+
 
 function App() {
+  
+  const [ data, setData ] = useState({});
+  
+  fetch(URL).then((response) => {
+    response.text().then((resData) => {
+      setData(resData)
+    })
+  })
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <code>{ JSON.stringify(data, null, 2)}</code>
         </p>
         <a
           className="App-link"
